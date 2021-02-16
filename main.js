fa8857f6d11eead6440865b5c23ab938923a8f0e
Tiffany <tlewis1287@gmail.com>
2021-02-15 23:16:16 -0500

    V2 Work Page
    -added background
    -added upper capabilities section

---
+++ main.js
@@ -0,0 +1,7 @@
+function changeColor(obj) {
+    if (obj.style.color == 'rgb(242, 198, 160)') {
+        obj.style.color = 'white';
+    } else {
+        obj.style.color = 'rgb(242, 198, 160)';
+    }
+}
